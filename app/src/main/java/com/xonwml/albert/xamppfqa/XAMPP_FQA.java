15f41e172712f5083667404e6767856259937f07
Albert Bytyçi <albertbytyci@gmail.com>
2016-11-22 08:36:56 +0100

    XonWML
    XonWML FQA

---
+++ app/src/main/java/com/xonwml/albert/xamppfqa/XAMPP_FQA.java
@@ -0,0 +1,13 @@
+package com.xonwml.albert.xamppfqa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class XAMPP_FQA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xampp__fq);
+    }
+}
